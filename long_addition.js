94c36f8b184a64674678c887ae82df839ad0b40b
DanielJDufour <daniel.j.dufour@gmail.com>
2022-01-27 22:52:51 -0500

    reformat

--- long_addition.js
+++ long_addition.js
@@ -4,7 +4,6 @@ module.exports = function long_addition(a, b) {
 
   const alen = a.length;
   const blen = b.length;
-  // console.log({alen, blen, maxlen});
 
   const aidx = a.indexOf(".");
   const bidx = b.indexOf(".");
@@ -14,7 +13,6 @@ module.exports = function long_addition(a, b) {
   // like 123.
   const a_adjusted_dot_index = aidx === -1 ? alen : aidx;
   const b_adjusted_dot_index = bidx === -1 ? blen : bidx;
-  // console.log({a_adjusted_dot_index, b_adjusted_dot_index});
 
   // how much you need to shift the second number
   // to line up the decimal with the first
@@ -22,20 +20,15 @@ module.exports = function long_addition(a, b) {
   //    12345.0
 
   const offset = a_adjusted_dot_index - b_adjusted_dot_index;
-  // console.log("offset:", offset);
 
   let left = Math.max(a_adjusted_dot_index, b_adjusted_dot_index);
-  // console.log("left:", left);
 
   let right = Math.max(alen - a_adjusted_dot_index - 1, blen - b_adjusted_dot_index - 1);
-  // console.log("right:", right);
 
   let aoffset = offset < 0 ? -1 * offset : 0;
   let boffset = offset <= 0 ? 0 : offset;
-  // console.log({aoffset, boffset});
 
   let imax = left + 1 + right - 1; // -1 for zero-index
-  // console.log({imax});
 
   let result = "";
 
@@ -47,10 +40,8 @@ module.exports = function long_addition(a, b) {
   let i = imax;
   if (right > 0) {
     while (i > imax - right) {
-      // console.log({i});
       const achar = a[i - aoffset] || "0";
       const bchar = b[i - boffset] || "0";
-      // console.log({achar, bchar});
       let n = Number(achar) + Number(bchar) + carried;
       if (n >= 10) {
         n -= 10;
@@ -67,14 +58,10 @@ module.exports = function long_addition(a, b) {
     i--; // substract 1 for dot
   }
 
-  // console.log({result});
-
   if (left > 0) {
     while (i >= 0) {
-      // console.log({i});
       const achar = a[i - aoffset] || "0";
       const bchar = b[i - boffset] || "0";
-      // console.log({achar, bchar});
       let n = Number(achar) + Number(bchar) + carried;
       if (n >= 10) {
         n -= 10;
@@ -83,23 +70,13 @@ module.exports = function long_addition(a, b) {
         carried = 0;
       }
       result = n + result;
-      // console.log({result});
       i--;
     }
   }
 
-  // console.log({result});
   if (carried === 1) {
     result = carried + result;
   }
-  // console.log({result});
 
   return result;
 };
-
-// add('12345.6789', '357910');
-
-// maxlen = 10
-
-//      12345.6789
-//     357910
