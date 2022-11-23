6a2f14f07e1ee24c68ec38486a1d0d853137c56b
DanielJDufour <daniel.j.dufour@gmail.com>
2022-11-22 22:13:54 -0500

    updated euler's number

--- eulers_number.js
+++ eulers_number.js
@@ -1,7 +1,7 @@
 const add = require("./add.js");
 const divide = require("./divide.js");
 
-function eulers_number(steps = 100, { max_decimal_digits = 100 } = {}) {
+function eulers_number({ max_decimal_digits = 100, steps = 100 } = {}) {
   let sum = "1";
   let step = "1";
   for (let i = 1; i < steps; i++) {
