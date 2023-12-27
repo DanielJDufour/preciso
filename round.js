01fa42108ed88ec045f5e2c0ab17639c72fd3fe3
DanielJDufour <daniel.j.dufour@gmail.com>
2023-12-26 22:11:18 -0500

    round accepts numerical string as digits option

--- round.js
+++ round.js
@@ -4,6 +4,9 @@ const is_imaginary = require("./is_imaginary.js");
 const round_rational = require("./round_rational.js");
 
 function round(n, { digits = 0 } = {}) {
+  // in case you pass in a numerical string for digits
+  digits = Number(digits);
+
   if (is_imaginary(n)) {
     return round_rational(n.substring(0, n.length - 1), { digits }) + "i";
   } else {
